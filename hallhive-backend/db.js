304a7556d8f1465cadf6dc3cd19ecfca0bb49975
shafasust <sumaiyashafa.15@gmail.com>
2025-06-14 17:53:33 +0600

    Add db.js

--- hallhive-backend/db.js
+++ hallhive-backend/db.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'halluser',
-  password: 'Sumaiya22shafa44@sql',
-  database: 'hallhive'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
+module.exports = db;
 // Test the database connection
 db.query('SELECT 1')
   .then(() => console.log('Database connected'))
